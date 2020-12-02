ff063c164ab5b76d667f3edb4642e5f00bdfb99c
DarkWe3d <71343008+DarkWe3d@users.noreply.github.com>
2020-12-02 06:05:48 -0500

    Update series_resto5.sql
    Cambios de solo los nombres a los atributos de las columnas

--- series_resto5.sql
+++ series_resto5.sql
@@ -1,21 +1,16 @@
-create table reto5.serie (
-	id_contenido int primary key auto_increment,
-    titulo_contenido varchar(50),
-    numTempo_contenido int(5),
-    numEp_contenido int(5)
-);
-INSERT INTO `reto5`.`serie`
-(`id_contenido`,
-`titulo_contenido`,
-`numTempo_contenido`,
-`numEp_contenido`
-)
-VALUES
-(null,'The walking dead',11,153),
-(null,'Viaje a las estrellas: la serie original',3,80),
-(null,'Glow',3,30),
-(null,'La casa de papel',4,31),
-(null,'Friends',10,236),
-(null,'Arrow',8,170),
-(null,'The big bang theory',12,279),
-(null,'Vikingos',6,79);
+CREATE TABLE serie(
+	ser_id int auto_increment,
+	ser_titulo varchar(100)  NOT NULL,
+    ser_temporadas int NOT NULL,
+    ser_episodios int NOT NULL,
+    Primary Key(ser_id)
+    );
+INSERT INTO serie(ser_titulo,ser_temporadas,ser_episodios)
+values ('The walking dead',11,153),
+('Viaje a las estrellas',3,80),
+ ('Glow',3,30),
+ ('La casa de papel',4,31),
+ ('Friends',10,236),
+ ('Arrow',8,170),
+ ('The big bang theory',12,279),
+ ('Vikingos',6,79);
